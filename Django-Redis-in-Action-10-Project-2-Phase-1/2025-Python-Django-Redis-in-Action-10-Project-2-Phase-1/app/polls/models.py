9fd20274b34be4fbdd5946ddd1ce31f07ff61e9e
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2026-03-18 08:39:27 +0530

    Delete Django-Redis-in-Action-10-Project-2-Phase-1/2025-Python-Django-Redis-in-Action-10-Project-2-Phase-1 directory

--- Django-Redis-in-Action-10-Project-2-Phase-1/2025-Python-Django-Redis-in-Action-10-Project-2-Phase-1/app/polls/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Poll(models.Model):
-    question = models.CharField(max_length=255)
-    text = models.JSONField()
-
-    def __str__(self):
-        return self.question
